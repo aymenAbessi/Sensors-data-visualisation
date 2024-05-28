d965c21ade2fbcd64111727da2d3975a6ad962b8
aymenAbessi <99690345+aymenAbessi@users.noreply.github.com>
2024-05-28 16:33:43 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAh9hpOPDZZX24jY5rWDsZkI86UBfUX8yA",
-  authDomain: "smarthome-f314e.firebaseapp.com",
-  databaseURL: "https://smarthome-f314e-default-rtdb.firebaseio.com",
-  projectId: "smarthome-f314e",
-  storageBucket: "smarthome-f314e.appspot.com",
-  messagingSenderId: "885584363406",
-  appId: "1:885584363406:web:6896cfca44086efa4beedf",
-  measurementId: "G-F5XTSZZ2VM"
+
 };
 
 export const app = initializeApp(firebaseConfig);
